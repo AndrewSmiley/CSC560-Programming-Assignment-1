3962e52b4b93c7c74ec402a93cc767a160d66bfd
Andrew Smiley <andrew.smiley@live.com>
2016-04-09 15:37:06 -0400

    Test

--- src/csc560/TestClient.java
+++ src/csc560/TestClient.java
@@ -2,46 +2,172 @@ package csc560;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by Andrew on 4/9/16.
  */
 public class TestClient {
     int counter = 0;
-    public static void main(String args[]) {
-        TestClient tc = new TestClient();
-        while (true) {
-            new Thread() {
-                @Override
-                public void run() {
-                    Socket socket = null;
-                    try {
-                        socket = new Socket("localhost", 7788);
-                        tc.counter++;
-                        while (true) {
-                            PrintWriter out = new PrintWriter(socket.getOutputStream(),
-                                    true);
+    int serverid = 666;
+    int clientid = 777;
 
-                            out.println("FUCK YOU FROM CLIENT "+tc.counter);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(
-                                    socket.getInputStream()));
-                            System.out.println(in.readLine());
-                            Thread.sleep(2000);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+    public void playGame() {
+        try {
 
+
+            System.out.println("Client (You) is O, Server is X.\nMoves are zero indexed, i.e. row 1 is row 0, row 2 is row 1, etc. Columns are the same way");
+            Socket socket = new Socket("localhost", 7788);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),
+                    true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+//            String message =
+            int[][] board = new int[3][3];
+            //get the inital message
+            String move = recieveMessage(in);
+            //if user makes the first move
+            if(move.contains("NONE")){
+                move = getUserMove();
+                sendMessage(out,move);
+                //just keep an eye on the board
+                processMove(board, move, clientid);
+                System.out.println("Client Turn");
+                printBoard(board);
+
+            }else{
+                //if server makes first move
+                processMove(board, move, serverid);
+                System.out.println("Server Turn");
+                printBoard(board);
+                //now we get the user move
+                move = getUserMove();
+                sendMessage(out,move);
+                //just keep an eye on the board
+                processMove(board, move, clientid);
+                System.out.println("Client Turn");
+                printBoard(board);
+
+            }
+
+            while(true){
+                //get the new move
+                move = recieveMessage(in);
+                //if the game is over, break
+                if(move.toLowerCase().contains("win") || move.toLowerCase().contains("loss") || move.toLowerCase().contains("tie")){
+                    processMove(board, move, serverid);
+                    System.out.println(move);
+                    break;
                 }
 
-            }.start();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                //if server makes first move
+                processMove(board, move, serverid);
+                System.out.println("Server Turn");
+                printBoard(board);
+
+                //now we get the user move
+                move = getUserMove();
+                sendMessage(out,move);
+                //just keep an eye on the board
+                processMove(board, move, clientid);
+                System.out.println("Client Turn");
+                printBoard(board);
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public void printBoard(int[][] board){
+        for(int i = 0; i < 3; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < 3; j++){
+//                sb.append("|");
+//                r = r.concat((board[i][j]== 0) ? "_" : (String) board[i][j]));
+                sb.append(String.valueOf((board[i][j]== 0) ? "_" : board[i][j]));
+                sb.append(" ");
+//                sb.append("|");
+            }
+
+            System.out.println(sb.toString().replace("666", "X").replace("777", "O"));
+        }
+        System.out.println("\n");
+    }
+    public String getUserMove(){
+        System.out.println("Your Turn");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.print("Enter a the row: ");
+        int r = reader.nextInt();
+        System.out.print("Enter a the column: ");
+        int c = reader.nextInt();
+
+
+
+        return "MOVE "+r+" "+c;
+    }
+    public void sendMessage(PrintWriter out, String message){
+        out.println(message);
+    }
+
+    public String recieveMessage(BufferedReader bf){
+        try {
+            return bf.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    void processMove(int[][] board, String move, int user ){
+        //get the rows and columns
+        String[] tmp = move.split(" ");
+        int row = Integer.parseInt(tmp[1]);
+        int column = Integer.parseInt(tmp[2]);
+        board[row][column] = user;
+
+    }
+
+
+
+    public static void main(String args[]) {
+        TestClient tc = new TestClient();
+        tc.playGame();
+//        TestClient tc = new TestClient();
+//        while (true) {
+//            new Thread() {
+//                @Override
+//                public void run() {
+//                    Socket socket = null;
+//                    try {
+//                        socket = new Socket("localhost", 7788);
+//                        tc.counter++;
+//                        while (true) {
+//                            PrintWriter out = new PrintWriter(socket.getOutputStream(),
+//                                    true);
+//
+//                            out.println("FUCK YOU FROM CLIENT "+tc.counter);
+//                            BufferedReader in = new BufferedReader(new InputStreamReader(
+//                                    socket.getInputStream()));
+//                            System.out.println(in.readLine());
+//                            Thread.sleep(2000);
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//
+//            }.start();
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
--- src/csc560/TestServer.java
+++ src/csc560/TestServer.java
@@ -3,12 +3,32 @@ package csc560;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Andrew on 4/9/16.
  */
 public class TestServer {
-
+    int serverid = 666;
+    int clientid = 777;
+    int tieflag = 6969;
+    int nowinnerflag=9999;
+    volatile int wins = 0;
+    volatile int losses = 0;
+    volatile int ties = 0;
+    boolean semaphore = false;
+    public ArrayList<String> getAvailableMoves(int[][] board){
+        ArrayList<String> moves = new ArrayList<String>();
+        for (int i =0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == 0){
+                    moves.add("MOVE "+i+" "+j);
+                }
+            }
+        }
+        return moves;
+    }
     class T implements Runnable{
         Socket socket;
         int count;
@@ -21,21 +41,202 @@ public class TestServer {
         public void run() {
             PrintWriter out = null;
             try {
-                while (true) {
+
                     out = new PrintWriter(socket.getOutputStream(),
                             true);
-                    out.println("FUCK YOU FROM SERVER "+count);
+
                     BufferedReader in = new BufferedReader(new InputStreamReader(
                             socket.getInputStream()));
-                    System.out.println(in.readLine());
-                    Thread.sleep(2000);
+
+                int[][] board = new int[3][3];
+                Random rand = new Random();
+                int randomNum = rand.nextInt((100 - 1) + 1) + 1;
+
+                if(randomNum % 2 == 0){
+                    sendMessage(out, "NONE");
+
+                }else{
+                    //generate the random server move
+                    int random = new Random().nextInt(getAvailableMoves(board).size());
+                    sendMessage(out, getAvailableMoves(board).get(random));
+                    processMove(board, getAvailableMoves(board).get(random), serverid);
+
                 }
+
+                while(true){
+                    //ok so now get the response from the client
+                    String move = recieveMessage(in);
+                    //process it
+                    processMove(board, move, clientid);
+                    //check if we have a winner
+                    int winlosstienowin = determineWinner(board);
+                    if (winlosstienowin != nowinnerflag){
+                        if(winlosstienowin == serverid){
+                            sendMessage(out, move+" LOSS");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            losses ++;
+                            semaphore = false;
+                        }
+                        if(winlosstienowin == clientid){
+                            sendMessage(out, move+" WIN");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            wins++;
+                            semaphore = false;
+                        }
+                        if(winlosstienowin == tieflag){
+                            sendMessage(out, move+" TIE");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            ties ++;
+                            semaphore = false;
+                        }
+                        printStats();
+                        break;
+                    }
+
+                    //do the server work
+                    int random = new Random().nextInt(getAvailableMoves(board).size());
+                    sendMessage(out, getAvailableMoves(board).get(random));
+                    processMove(board, getAvailableMoves(board).get(random), serverid);
+                    winlosstienowin = determineWinner(board);
+                    if (winlosstienowin != nowinnerflag){
+                        if(winlosstienowin == serverid){
+                            sendMessage(out, move+" LOSS");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            losses ++;
+                            semaphore = false;
+                        }
+                        if(winlosstienowin == clientid){
+                            sendMessage(out, move+" WIN");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            wins++;
+                            semaphore = false;
+                        }
+                        if(winlosstienowin == tieflag){
+                            sendMessage(out, move+" TIE");
+                            while (semaphore){
+                                ;
+                            }
+                            //aquire
+                            semaphore = true;
+                            ties ++;
+                            semaphore = false;
+                        }
+                        break;
+                    }
+                }
+
             } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    public void printStats(){
+        while(semaphore){
+            ;
+        }
+        //aquire lock
+        semaphore = true;
+        System.out.println("Wins: "+wins);
+        System.out.println("Losses: "+losses);
+        System.out.println("Ties: "+ties);
+        semaphore = false;
+
+    }
+    public int determineWinner(int[][] board){
+        //check to see if the rows are the same
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][2]){
+            if (board[0][0] != 0) {
+                return board[0][0];
+            }
+        }
+        if(board[1][0] == board[1][1] && board[1][1] == board[1][2]){
+            if (board[1][0] != 0) {
+                return board[1][0];
+            }
+        }
+        if(board[2][0] == board[2][1] && board[2][1] == board[2][2]){
+            if (board[2][0]!=0) {
+                return board[2][0];
+            }
+        }
+
+        //now check the columns
+        if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+            if (board[0][0] != 0) {
+                return board[0][0];
+            }
+        }
+
+        if(board[0][1] == board[1][1] && board[1][1] == board[2][1]){
+            if (board[0][1]!=0) {
+                return board[0][1];
+            }
+        }
+        if(board[0][2] == board[1][2] && board[1][2] == board[2][2]){
+            if (board[0][2]!=0) {
+                return board[0][2];
+            }
+        }
+
+        //now check the diagonals
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            if (board[0][0] != 0) {
+                return board[0][0];
+            }
+        }
+
+            if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+                if (board[0][2]!=0) {
+                    return board[0][2];
+                }
+        }
+
+        if(getAvailableMoves(board).size() == 0){
+            return tieflag;
+        }
+
+        return nowinnerflag;
+    }
+    void processMove(int[][] board, String move, int user ){
+        //get the rows and columns
+        String[] tmp = move.split(" ");
+        int row = Integer.parseInt(tmp[1]);
+        int column = Integer.parseInt(tmp[2]);
+        board[row][column] = user;
+
+    }
+    public void sendMessage(PrintWriter out, String message){
+        out.println(message);
+    }
+
+    public String recieveMessage(BufferedReader bf){
+        try {
+            return bf.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 
